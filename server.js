7f438c9edd52fcb0a583561ff0ff2d80ab57d441
mariyask04 <skmariya484@gmail.com>
2026-01-08 18:41:09 +0530

    added deployed frontend url

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: ["http://localhost:3000", "https://movieappfrontend.vercel.app"] }));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
